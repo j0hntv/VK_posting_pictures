201b1b7c9a2df9ad94c2a05c9067b6b34e99e778
J0hn <j0hntvdesign@gmail.com>
2019-07-25 08:26:06 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import requests
 import random
 import logging
+import os
+from dotenv import load_dotenv
 
 logging.basicConfig(level=logging.INFO, format='[%(levelname)s] - %(message)s')
 
@@ -26,13 +28,23 @@ def get_comics_data(comics_number):
     return (url, title, alt)
 
 
+def publish_image(image):
+    pass
+    logging.info(f'<{image}> pulished.')
+
+
 def main():
     random_image_number = random.randint(1, 1000)
     url, title, alt = get_comics_data(random_image_number)
     name = url.split('/')[-1]
     save_image(url, name)
-    print(title)
-    print(alt)
+    logging.info(f'Title: {title}')
+    logging.info(f'Alt: {alt}')
+    publish_image(name)
+
+    os.remove(name)
+
+    logging.info(f'<{name}> removed.')
 
 
 if __name__ == "__main__":
