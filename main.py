15580178aab5aa508931bf2bd6ad979fb8e5c287
J0hn <j0hntvdesign@gmail.com>
2019-07-31 10:13:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,6 @@ from dotenv import load_dotenv
 logging.basicConfig(level=logging.INFO, format='[%(levelname)s] - %(message)s')
 
 
-def save_image(url, name):
-    response = requests.get(url)
-    response.raise_for_status()
-    image = response.content
-    with open(name, 'wb') as image_file:
-        image_file.write(image)
-    logging.info(f'Saved <{name}>')
-
-
 def get_comics_data(comics_number):
     url = f'https://xkcd.com/{comics_number}/info.0.json'
     response = requests.get(url)
@@ -25,27 +16,31 @@ def get_comics_data(comics_number):
     url = json.get('img')
     title = json.get('safe_title')
     alt = json.get('alt')
+    logging.info(f'Image URL: {url}')
+    logging.info(f'Title: {title}')
+    logging.info(f'Alt: {alt}')
     return (url, title, alt)
 
 
-def publish_image(image):
-    pass
-    logging.info(f'<{image}> pulished.')
+def save_image(url, name):
+    response = requests.get(url)
+    response.raise_for_status()
+    image = response.content
+    with open(name, 'wb') as image_file:
+        image_file.write(image)
+    logging.info(f'Saved <{name}>')
 
 
 def main():
     random_image_number = random.randint(1, 1000)
     url, title, alt = get_comics_data(random_image_number)
-    name = url.split('/')[-1]
-    save_image(url, name)
-    logging.info(f'Title: {title}')
-    logging.info(f'Alt: {alt}')
-    publish_image(name)
-
-    os.remove(name)
-
-    logging.info(f'<{name}> removed.')
+    image_name = url.split('/')[-1]
+    save_image(url, image_name)
 
 
 if __name__ == "__main__":
+    load_dotenv()
+    GROUP_ID = os.getenv('GROUP_ID')
+    ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
     main()
+    
